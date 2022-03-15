2ade252e004b1fd47ce2127ad09086d6d7d370b7
Patrick Radulian <patrick.radulian@gmail.com>
2022-03-15 16:22:05 +0100

    Replace placeholder elements in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Bar from 'components/bar/bar';
+import Separator from 'components/separator/separator';
+import IconButton from 'components/icon-button/icon-button';
+import Dashboard from "./icons/dashboard.svg";
+import BarItemGroup from 'components/bar-item-group/bar-item-group';
 
 const App: React.FC<{}> = () => {
     return (
         <div className="App">
             <main className="main">
-                Placeholder
+                <Bar>
+                    <BarItemGroup justify="left">
+                        <IconButton icon={Dashboard} alt="Dashboard"/>
+                    </BarItemGroup>
+
+                    <BarItemGroup justify="right">
+                        <span>Discard Changes</span>
+                    </BarItemGroup>
+                </Bar>
+
+                <Separator/>
             </main>
         </div>
     );
