a073a10694b6bcc71d0752fc72506158848e6177
Patrick Radulian <patrick.radulian@gmail.com>
2022-03-15 17:34:14 +0100

    Adjust bar component props

--- src/components/bar/bar.tsx
+++ src/components/bar/bar.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import styles from "./bar.module.css";
 
 type BarProps = {
-    children?: React.ReactElement | Array<React.ReactElement>;
+    children?: React.ReactElement | Array<React.ReactElement>,
+    height?: number
 }
 
 const Bar: React.FC<BarProps> = (props: BarProps) => {
     return (
-        <div className={styles.bar}>{props.children}</div>
+        <div className={styles.bar} style={{height: props.height}}>{props.children}</div>
     )
 }
 
