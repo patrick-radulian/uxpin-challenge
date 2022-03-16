16111ff332103a73774b4b4709568c77c80744d1
Patrick Radulian <patrick.radulian@gmail.com>
2022-03-16 19:15:11 +0100

    Adjust separator styling props

--- src/components/atoms/separator/separator.tsx
+++ src/components/atoms/separator/separator.tsx
@@ -5,13 +5,15 @@ import styles from "./separator.module.css";
 type SeparatorProps = {
     color?: CSS.Property.Color
     vertical?: boolean
+    margin?: CSS.Property.Margin
 }
 
 const Separator: React.FC<SeparatorProps> = (props: SeparatorProps) => {
     const style: CSS.Properties = {
         color: props.color,
         height: props.vertical ? "100%" : "1px",
-        width: props.vertical ? "1px" : "100%"
+        width: props.vertical ? "1px" : "100%",
+        margin: props.margin
     }
 
     return (
