d11982b10079eb63a193b71bf1817c8daad0e361
Patrick Radulian <patrick.radulian@gmail.com>
2022-03-17 09:17:34 +0100

    Add properties component

---
+++ src/components/organisms/properties/properties.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+type PropertiesProps = {
+
+}
+
+const Properties: React.FC<PropertiesProps> = (props: PropertiesProps) => {
+    return (
+        <div>Properties</div>
+    )
+}
+
+export default Properties;
