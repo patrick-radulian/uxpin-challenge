d598cc546971063d183ed1daa6125f11a33911e1
Patrick Radulian <patrick.radulian@gmail.com>
2022-03-17 10:52:43 +0100

    Add btn-size and icon-size props to icon-button

--- src/components/atoms/icon-button/icon-button.tsx
+++ src/components/atoms/icon-button/icon-button.tsx
@@ -5,13 +5,29 @@ import { ReactSVG } from 'react-svg';
 
 type IconButtonProps = {
     iconSrc: string
-    style?: CSS.Properties
+
+    height?: CSS.Property.Height<string | number>
+    width?: CSS.Property.Width<string | number>
+    iconHeight?: CSS.Property.Height<string | number>
+    iconWidth?: CSS.Property.Width<string | number>
 }
 
 const IconButton: React.FC<IconButtonProps> = (props: IconButtonProps) => {
+    const btnStyle: CSS.Properties<string | number> = {
+        height: props.height,
+        width: props.width
+    }
+
+    const setSvgStyle = (svg: SVGSVGElement) => {
+        const width = props.iconWidth ? `width: ${props.iconWidth}px` : "";
+        const height = props.iconHeight ? `height: ${props.iconHeight}px` : "";
+
+        return svg.setAttribute("style", `${width}; ${height};`);
+    }
+
     return (
-        <button className={styles['icon-button']} style={props.style}>
-            <ReactSVG src={props.iconSrc}/>
+        <button className={styles['icon-button']} style={btnStyle}>
+            <ReactSVG src={props.iconSrc} beforeInjection={svg => setSvgStyle(svg)}/>
         </button>
     )
 }
