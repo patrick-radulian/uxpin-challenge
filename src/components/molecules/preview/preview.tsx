81d8cf6304bf9ed62902cab934f62b3cb94e325a
Patrick Radulian <patrick.radulian@gmail.com>
2022-03-17 09:17:28 +0100

    Add preview component

---
+++ src/components/molecules/preview/preview.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+type PreviewProps = {
+
+}
+
+const Preview: React.FC<PreviewProps> = (props: PreviewProps) => {
+    return (
+        <div>Preview</div>
+    )
+}
+
+export default Preview;
