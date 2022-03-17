c99a28846476bf35b51a32921a7a37eb204867bd
Patrick Radulian <patrick.radulian@gmail.com>
2022-03-17 10:16:16 +0100

    Fix changed image import in top-bar

--- src/components/organisms/top-bar/top-bar.tsx
+++ src/components/organisms/top-bar/top-bar.tsx
@@ -1,7 +1,7 @@
 import Bar from "components/atoms/bar/bar";
 import React from "react";
 import IconButton from 'components/atoms/icon-button/icon-button';
-import Dashboard from "icons/dashboard.svg";
+import LayoutMediumTile from "icons/layout-medium-tile.svg";
 import BarItemGroup from 'components/atoms/bar-item-group/bar-item-group';
 import Breadcrumbs from 'components/atoms/breadcrumbs/breadcrumbs';
 import Link from 'components/atoms/link/link';
@@ -15,7 +15,7 @@ const TopBar: React.FC<{}> = () => {
     return (
         <Bar style={{padding: "8px", alignItems: "center"}}>
             <BarItemGroup justify="left">
-                <IconButtonWithTooltip iconSrc={Dashboard} alt="Dashboard" tooltip='Open Dashboard' tooltipPosition='bottom'/>
+                <IconButtonWithTooltip iconSrc={LayoutMediumTile} alt="Dashboard" tooltip='Open Dashboard' tooltipPosition='bottom'/>
                 <Separator vertical margin="0 16px 0 11px"/>
 
                 <Breadcrumbs crumbs={["Material UI", "Button"]}/>
