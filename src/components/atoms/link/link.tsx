c914425c4ee367445283c15550a4c00604536d31
Patrick Radulian <patrick.radulian@gmail.com>
2022-03-18 11:47:53 +0100

    Add icon prop and options to link component

--- src/components/atoms/link/link.tsx
+++ src/components/atoms/link/link.tsx
@@ -1,16 +1,31 @@
 import React from "react";
+import * as CSS from "csstype";
 import styles from "./link.module.css";
+import SVG from "react-inlinesvg";
 
 type LinkProps = {
-    children: React.ReactElement | string
+    children: React.ReactElement | string | Array<React.ReactElement | string>
     linkTo: string
+    underline?: boolean
+    iconSrc?: string
+    iconPosition?: "start" | "end"
+    iconSize?: number
+    style?: CSS.Properties
 }
 
-const Link: React.FC<LinkProps> = (props: LinkProps) => {
+const Link: React.FC<LinkProps> = ({children, linkTo, underline = true, iconSrc, iconPosition = "start", iconSize = 12, style}: LinkProps) => {
+    const icon = iconSrc ? <SVG src={iconSrc} height={iconSize} width={iconSize} fill="currentColor"/> : null;
+
     return (
-        <a className={styles.link} href={props.linkTo}>
-            <span>{props.children}</span>
-            <div className={styles["link-decoration"]}></div>
+        <a className={styles.link} href={linkTo} style={style}>
+            {icon && iconPosition === "start" && icon}
+
+            <div className={styles["link-text"]}>
+                {children}
+                {underline && <div className={styles["link-decoration"]}></div>}
+            </div>
+
+            {icon && iconPosition === "end" && icon}
         </a>
     )
 }
--- src/components/organisms/properties/properties.tsx
+++ src/components/organisms/properties/properties.tsx
@@ -1,5 +1,8 @@
+import Link from "components/atoms/link/link"
+import Typography from "components/atoms/typography/typography"
 import React from "react"
 import styles from "./properties.module.css"
+import Plus from "icons/plus.svg";
 
 type PropertiesProps = {
 
@@ -8,7 +11,11 @@ type PropertiesProps = {
 const Properties: React.FC<PropertiesProps> = (props: PropertiesProps) => {
     return (
         <div className={styles.properties}>
-            <div className={styles.header}></div>
+            <div className={styles.header}>
+                <Typography variant="h4">Properties</Typography>
+
+                <Link linkTo="" iconSrc={Plus} iconSize={8} style={{color: "var(--blue-base)"}}>Add new property</Link>
+            </div>
         </div>
     )
 }
