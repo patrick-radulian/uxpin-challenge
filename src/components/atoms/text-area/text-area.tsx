2dab3a82ea291a6bc2c006f340e4edd79bfe5ed3
Patrick Radulian <patrick.radulian@gmail.com>
2022-03-19 17:26:49 +0100

    Adjust textarea rendering, add hint and row props

--- src/components/atoms/text-area/text-area.tsx
+++ src/components/atoms/text-area/text-area.tsx
@@ -1,16 +1,23 @@
 import React from "react"
 import * as CSS from "csstype"
 import styles from "./text-area.module.css"
+import Typography from "../typography/typography"
 
 type TextAreaProps = {
     value: string
     onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void
+    rows?: number
+    hint?: string
     style?: CSS.Properties
 }
 
-const TextArea: React.FC<TextAreaProps> = ({value, onChange, style}: TextAreaProps) => {
+const TextArea: React.FC<TextAreaProps> = ({value, onChange, rows = 3, hint, style}: TextAreaProps) => {
     return (
-        <textarea onChange={onChange} className={styles["text-area"]} value={value} style={style}/>
+        <React.Fragment>
+            <textarea onChange={onChange} className={styles["text-area"]} value={value} style={style} rows={rows}/>
+
+            {hint && <div className={styles.hint}><Typography variant="small">{hint}</Typography></div>}
+        </React.Fragment>
     )
 }
 
