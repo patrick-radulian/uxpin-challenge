c870e5fffd5a24b7d05dabc378dc975b98853c43
Patrick Radulian <patrick.radulian@gmail.com>
2022-03-20 16:35:49 +0100

    Add optional style prop to switch component

--- src/components/atoms/switch/switch.tsx
+++ src/components/atoms/switch/switch.tsx
@@ -1,15 +1,17 @@
 import React from "react"
+import * as CSS from "csstype"
 import Separator from "../separator/separator"
 import styles from "./switch.module.css"
 
 type SwitchProps = {
     checked: boolean
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+    style?: CSS.Properties
 }
 
-const Switch: React.FC<SwitchProps> = ({checked, onChange}: SwitchProps) => {
+const Switch: React.FC<SwitchProps> = ({checked, onChange, style}: SwitchProps) => {
     return (
-        <div className={styles.switch}>
+        <div className={styles.switch} style={style}>
             <input onChange={onChange} type="checkbox" checked={checked}/>
             <div className={`${styles["switch-value"]} ${checked ? "checked" : ""}`}>True</div>
             <Separator vertical/>
