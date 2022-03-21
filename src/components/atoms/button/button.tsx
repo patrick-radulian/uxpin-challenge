33852bfebf1df2818dccf52e8369ad9a1854b054
Patrick Radulian <patrick.radulian@gmail.com>
2022-03-21 08:32:21 +0100

    Redesign icon implementation in button component

--- src/components/atoms/button/button.tsx
+++ src/components/atoms/button/button.tsx
@@ -8,18 +8,20 @@ type ButtonProps = {
     weight?: "light" | "heavy"
     size?: "small" | "large"
     stretched?: boolean
-    iconSrc?: string
+    icon?: React.ReactElement
+    iconPosition?: "start" | "end"
     disabled?: boolean
     style?: CSS.Properties
+    className?: string
     onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
 }
 
 const Button: React.FC<ButtonProps> = ({children, variant = "default", weight = "heavy", size, ...rest}: ButtonProps) => {
-    const className: string = `${style.button} ${variant} ${weight} ${size ? size : ""} ${rest.stretched ? "stretched" : ""}`;
+    const btnClassName: string = `${style.button} ${variant} ${weight} ${size ? size : ""} ${rest.stretched ? "stretched" : ""} ${rest.className ? rest.className : ""}`;
 
     return (
-        <button className={className} onClick={rest.onClick} style={rest.style}>
-            {rest.iconSrc && <img src={rest.iconSrc} alt=""/>}
+        <button className={btnClassName} onClick={rest.onClick} style={rest.style}>
+            {rest.icon && rest.icon}
             {children}
         </button>
     )
