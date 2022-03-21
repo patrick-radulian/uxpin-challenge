2e11e25f99ca39536b5fd038845f1c43f340b27e
Patrick Radulian <patrick.radulian@gmail.com>
2022-03-21 08:34:07 +0100

    Add zoom-control component

---
+++ src/components/atoms/zoom-control/zoom-control.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import * as CSS from "csstype"
+import styles from "./zoom-control.module.css"
+import Typography from "../typography/typography"
+import Input from "../input/input"
+
+type ZoomControlProps = {
+    zoom: number
+    onChange: (e: ZoomType | React.ChangeEvent<HTMLInputElement>) => void
+    style?: CSS.Properties
+}
+
+export type ZoomType = "in" | "out"
+
+const ZoomControl: React.FC<ZoomControlProps> = ({zoom, onChange, style}: ZoomControlProps) => {
+    const onZoomOut = React.useCallback(() => onChange("out"), [onChange]);
+    const onZoomIn = React.useCallback(() => onChange("in"), [onChange]);
+
+    return (
+        <div className={styles["zoom-control"]} style={style}>
+            <div onClick={onZoomOut} className={styles["zoom-action"]}>-</div>
+            <Input onChange={onChange} value={`${zoom}%`} className={styles["zoom-status"]}/>
+            {/* <Typography className={styles["zoom-status"]} style={{lineHeight: "28px"}}>{`${zoom}%`}</Typography> */}
+            <div onClick={onZoomIn} className={styles["zoom-action"]}>+</div>
+        </div>
+    )
+}
+
+export default ZoomControl;
