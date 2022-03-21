cf093f6344ee9ee7a163c00fc22b9704df4ef064
Patrick Radulian <patrick.radulian@gmail.com>
2022-03-21 08:31:09 +0100

    Prepare app structure for uxpin header

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,28 @@ import TopBar from 'components/organisms/top-bar/top-bar';
 import Header from 'components/molecules/header/header';
 import Preview from 'components/molecules/preview/preview';
 import Properties from 'components/organisms/properties/properties';
+import UXPinHeader from 'components/molecules/uxpin-header/uxpin-header';
 
 const App: React.FC<{}> = () => {
     return (
-        <div className="App">
-            <div className="main">
-                <TopBar/>
-                <Separator color="var(--silver-darken5)"/>
-                <Header/>
-                <Preview/>
-                <Properties/>
-            </div>
+        <React.Fragment>
+            <UXPinHeader/>
 
-            <Separator vertical/>
-        </div>
+            <div className="App-scroll-container">
+                <div className="App">
+                    <div className="main">
+                        <TopBar/>
+                        <Separator color="var(--silver-darken5)"/>
+                        <Header/>
+                        <Preview/>
+                        <Properties/>
+                    </div>
+
+                    <Separator vertical/>
+                </div>
+            </div>
+        </React.Fragment>
+
     );
 }
 
